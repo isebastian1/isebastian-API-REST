c703f80f3f3d1fb505e54f39174fb8a6305fc913
ISebastian <irving.gregorio20@gmail.com>
2026-03-22 16:31:54 -0600

    Create Project

---
+++ src/main/java/com/chakray/isebastian/IsebastianApplication.java
@@ -0,0 +1,13 @@
+package com.chakray.isebastian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsebastianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsebastianApplication.class, args);
+	}
+
+}
