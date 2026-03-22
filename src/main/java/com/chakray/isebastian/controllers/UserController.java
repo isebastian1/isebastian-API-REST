1a017e58dcb615daf040b5342190f45451dc47a9
ISebastian <irving.gregorio20@gmail.com>
2026-03-22 17:03:35 -0600

    Create Controllers

---
+++ src/main/java/com/chakray/isebastian/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.chakray.isebastian.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.chakray.isebastian.interfaces.UserService;
+import com.chakray.isebastian.models.User;
+
+//Controlador
+@RestController
+@CrossOrigin
+public class UserController {
+	//Inyectar servicio
+	@Autowired
+    private UserService userSrv;
+		
+	//Endpoint para obtener todos los usuarios
+    @GetMapping("/users")
+	public ResponseEntity<List<User>> users(){
+    	//Devolver un Response OK
+    	return ResponseEntity.ok(userSrv.getUsers());
+    }
+}
