b08c1f84c7baec4a90b4b67306428c192a373f43
ISebastian <irving.gregorio20@gmail.com>
2026-03-22 16:59:16 -0600

    Create Services

---
+++ src/main/java/com/chakray/isebastian/services/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.chakray.isebastian.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.chakray.isebastian.interfaces.UserService;
+import com.chakray.isebastian.models.User;
+
+//Servicio de usuarios, implementación
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public List<User> getUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
