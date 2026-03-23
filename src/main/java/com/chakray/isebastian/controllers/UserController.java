f1c29d400ab826d81ed46880e5e2f5bb8baf19e2
ISebastian <irving.gregorio20@gmail.com>
2026-03-22 18:29:39 -0600

    Modify ServiceImpl & Controller - Se crea un método para llenar el Array al iniciar la app, se implementa el método para ordenar los usuarios de acuerdo al atributo, se añade el request Param al Endpoint, y se agrega validación para el parametro sortedBy

--- src/main/java/com/chakray/isebastian/controllers/UserController.java
+++ src/main/java/com/chakray/isebastian/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.chakray.isebastian.interfaces.UserService;
+import com.chakray.isebastian.models.FilterAttribute;
 import com.chakray.isebastian.models.User;
 
 //Controlador
@@ -19,8 +20,13 @@ public class UserController {
 		
 	//Endpoint para obtener todos los usuarios
     @GetMapping("/users")
-	public ResponseEntity<List<User>> users(){
-    	//Devolver un Response OK
-    	return ResponseEntity.ok(userSrv.getUsers());
+	public ResponseEntity<List<User>> users(@RequestParam (required = false) FilterAttribute sortedBy){
+    	if(sortedBy != null) {
+    		//Devolver un Response OK
+    		return ResponseEntity.ok(userSrv.getUsersSortedBy(sortedBy));
+    	} else {
+    		//Devolver un Response OK
+    		return ResponseEntity.ok(userSrv.getUsers());
+    	}
     }
 }
