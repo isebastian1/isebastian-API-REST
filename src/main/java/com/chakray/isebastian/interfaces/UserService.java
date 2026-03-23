66298281618db1d5570f12b55f7e5240f7a730ff
ISebastian <irving.gregorio20@gmail.com>
2026-03-22 18:26:31 -0600

    Modify UserService - Se crea el nuevo método para ordenar los usuarios de acuerdo al atributo recibido

--- src/main/java/com/chakray/isebastian/interfaces/UserService.java
+++ src/main/java/com/chakray/isebastian/interfaces/UserService.java
@@ -2,10 +2,14 @@ package com.chakray.isebastian.interfaces;
 
 import java.util.List;
 
+import com.chakray.isebastian.models.FilterAttribute;
 import com.chakray.isebastian.models.User;
 
 //Interface para el Servicio de Usuarios
 public interface UserService {
 	//Obtener todos los usuarios
 	public List<User> getUsers();
+	
+	//Obtener usuarios ordenados de acuerdo al atributo sortedBy
+	public List<User> getUsersSortedBy(FilterAttribute sortedBy);
 }
