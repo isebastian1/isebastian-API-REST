e1b31dffb0aaef512ee8473f8ea010280c443606
ISebastian <irving.gregorio20@gmail.com>
2026-03-23 00:21:41 -0600

    Modify User - Se modifican las propiedades de los atributos, además se deja en el constructor la asignación de ID y fecha de creación

--- src/main/java/com/chakray/isebastian/models/User.java
+++ src/main/java/com/chakray/isebastian/models/User.java
@@ -1,8 +1,11 @@
 package com.chakray.isebastian.models;
 
+import java.util.List;
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.*;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -11,37 +14,45 @@ import java.time.format.DateTimeFormatter;
 //Clase Usuario
 public class User {
 	//Atributos
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
 	private UUID id;
+	@Email 
 	private String email;
+	@NotNull
 	private String name;
+	@NotNull
 	private String phone;
 	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@NotNull
 	private String password;
+	@NotNull
 	private String tax_id;
 	private String created_at;
-	private Address[] addresses;
+	@NotNull
+	private List<Address> addresses;
 	
 	//Métodos
 	//Constructor
-	public User(String email, String name, String phone, String password, String tax_id, Address[] addresses) {
-        this.setId();
+	public User(String email, String name, String phone, String password, String tax_id, List<Address> addresses) {
+		//Declarar el formato de la fecha
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-YYYY HH:mm");
+		
+		//Formatear la fecha de Madagascar y almacenarla en String
+		String created_at = ZonedDateTime.now(ZoneId.of("Indian/Antananarivo")).format(format);
+		
+        this.id = UUID.randomUUID(); //Asignarle un dato UUID aleatorio 
         this.setEmail(email);
         this.setName(name);
         this.setPhone(phone);
         this.setPassword(password);
         this.setTax_id(tax_id);
-        this.setCreated_at();
+        this.created_at = created_at;
         this.setAddresses(addresses);
 	}
 	
 	//Getters & Setters
 	public UUID getId() {
 		return id;
-	}
-
-	private void setId() {
-		//Asignarle un dato UUID aleatorio 
-		this.id = UUID.randomUUID();
 	}
 
 	public String getEmail() {
@@ -68,7 +79,7 @@ public class User {
 		this.phone = phone;
 	}
 
-	private void setPassword(String password) {
+	public void setPassword(String password) {
 		this.password = password;
 	}
 	
@@ -88,20 +99,12 @@ public class User {
 		return created_at;
 	}
 
-	private void setCreated_at() {
-		//Declarar el formato de la fecha
-		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-mm-yyyy HH:mm");
-		//Formatear la fecha de Madagascar y almacenarla en String
-		String created_at = ZonedDateTime.now(ZoneId.of("Indian/Antananarivo")).format(format);
-		
-		this.created_at = created_at;
-	}
-
-	public Address[] getAddresses() {
+	public List<Address> getAddresses() {
 		return addresses;
 	}
+	
 
-	public void setAddresses(Address[] addresses) {
+	public void setAddresses(List<Address> addresses) {
 		this.addresses = addresses;
 	}
 }
